ca365521b02e3d5db4ca1a9398e247b9aad40239
Hemant1803 <hshemantsharma445@gmail.com>
2023-09-28 00:06:12 +0530

    implement interface

---
+++ Interface.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Practice_Problem
+{
+    internal class Interface
+    {
+        public interface Employees
+        {
+            string Name { get; set; }
+            decimal Salary { get; set; }
+
+            void DisplayInfo();
+        }
+
+        
+        public class Employee : Employees
+        {
+            public string Name { get; set; }
+            public decimal Salary { get; set; }
+
+            public Employee(string name, decimal salary)
+            {
+                Name = name;
+                Salary = salary;
+            }
+
+            public void DisplayInfo()
+            {
+                Console.WriteLine($"Employee Name: {Name}");
+                Console.WriteLine($"Employee Salary: {Salary}");
+            }
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static OOP_Practice_Problem.Interface;
 
 namespace OOP_Practice_Problem
 {
@@ -6,16 +7,42 @@ namespace OOP_Practice_Problem
     {
        public static void Main(string[] args)
         {
+            for(; ; ) {
+                Console.WriteLine("****************************************************************************************************");
+                Console.WriteLine("Select the Program You Want To Run");
+            Console.WriteLine("\n1.Demonstrate different types of constructors. \n2.Perform interface concept with examples.\n3.Perform access modifiers concepts with examples. ");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        Constructors person1 = new Constructors();
+                        person1.Display();
+
+                        Constructors person2 = new Constructors("Prashant", 6000);
+                        person2.Display();
+                        break;
+
+                    case 2:
+                        Employees employee = new Employee("Hemant", 50000);
+
+                        employee.DisplayInfo();
 
 
-            Constructors person1 = new Constructors();
-            person1.Display(); 
+                        Console.WriteLine($"Employee Name: {employee.Name}");
+                        Console.WriteLine($"Employee Salary: {employee.Salary}");
+                        break;
+                    case 3:
+                        Console.WriteLine("hello");
+                        break;
+                    case 4:
+                        return;
 
-            Constructors person2 = new Constructors("Prashant", 6000);
-            person2.Display();
 
-            
 
+                }
+
+            }
         }
     }
 }
