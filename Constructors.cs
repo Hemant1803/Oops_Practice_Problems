527b323a6cf99330c981dee0c00ed46d4832680c
Hemant1803 <hshemantsharma445@gmail.com>
2023-09-27 23:31:29 +0530

    Demonstrate constructors

---
+++ Constructors.cs
@@ -0,0 +1,35 @@
+﻿using OOP_Practice_Problem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Practice_Problem
+{
+    internal class Constructors
+    {
+       
+              string name;
+              int salary;
+
+      
+        public Constructors()
+        {
+            name = "Hemant";
+            salary = 50000;
+        }
+
+        // Parameterized Constructor
+        public Constructors(string name, int salary)
+        {
+            this.name = name;
+            this.salary = salary;
+        }
+        public void Display()
+        {
+            Console.WriteLine($"Name: {name}, Salary: {salary}");
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace OOP_Practice_Problem
+{
+    internal class Program
+    {
+       public static void Main(string[] args)
+        {
+
+
+            Constructors person1 = new Constructors();
+            person1.Display(); 
+
+            Constructors person2 = new Constructors("Prashant", 6000);
+            person2.Display();
+
+            
+
+        }
+    }
+}
